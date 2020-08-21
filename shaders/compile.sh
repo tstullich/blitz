8beeccbb42c4ac1963997c6e49177215f6093604
Tim Stullich <tstullich@protonmail.com>
2020-08-20 21:19:44 -0700

    Updated the compile script a little bit

--- shaders/compile.sh
+++ shaders/compile.sh
@@ -1,3 +1,11 @@
-# TODO Make this a bit more useful
-/home/tim/lib/vulkan/1.2.141.2/x86_64/bin/glslc shader.vert -o vert.spv
-/home/tim/lib/vulkan/1.2.141.2/x86_64/bin/glslc shader.frag -o frag.spv
+#!/usr/bin/bash
+if ! command -v glslc &> /dev/null
+then
+  echo "glslc is not available on your system! Make sure to install it."
+  echo "Alternatively you can use LunarG's glslLangValidator."
+else
+  echo "Compiling shaders"
+  # By GLSL convention we search for the following files that have the given extensions below
+  glslc shader.vert -o vert.spv
+  glslc shader.frag -o frag.spv
+fi
